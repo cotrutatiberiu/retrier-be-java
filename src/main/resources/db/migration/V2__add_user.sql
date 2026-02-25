def170fab61fa47e0b39dd99fb3d92095fa6da90
Tiberiu Cotruta <cotruta.tiberiu@gmail.com>
2026-02-25 21:42:40 +0200

    update settings

--- src/main/resources/db/migration/V2__add_user.sql
+++ src/main/resources/db/migration/V2__add_user.sql
@@ -1,2 +1,2 @@
-INSERT INTO users (first_name, last_name, email, password, role)
+INSERT INTO tr.users (first_name, last_name, email, password, role)
 VALUES ('testFirstname', 'testLastname', 'testEmail', 'testPassword1234', 'USER');
