17005b132498d85ec99f4957cf9757b55cfcea38
Tiberiu Cotruta <cotruta.tiberiu@gmail.com>
2026-05-01 22:03:41 +0300

    refactor exceptio handlers

--- src/main/java/com/trier/trier_report/config/GlobalExceptionHandler.java
+++ src/main/java/com/trier/trier_report/config/GlobalExceptionHandler.java
@@ -1,54 +1,69 @@
 package com.trier.trier_report.config;
 
-import com.trier.trier_report.dto.ErrorResponse;
-import com.trier.trier_report.exception.AccessTokenExpiredException;
+import com.trier.trier_report.dto.ApiError;
 import com.trier.trier_report.exception.EmailUsedException;
-import com.trier.trier_report.exception.RefreshTokenExpiredException;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private ResponseEntity<ApiError> buildResponse(HttpStatus status, String message, WebRequest request) {
+        String path = request.getDescription(false).replace("uri=", "");
+        ApiError response = new ApiError(status.value(), message, path);
+
+        return new ResponseEntity<>(response, status);
+    }
+
     @ExceptionHandler(EmailUsedException.class)
-    public ResponseEntity<ErrorResponse> handleEmailAlreadyRegisteredException(EmailUsedException ex) {
-        ErrorResponse response = new ErrorResponse(409);
-        response.setErrors(new String[]{ex.getMessage()});
-
-        return ResponseEntity.status(response.getStatus()).body(response);
+    public ResponseEntity<ApiError> handleException(EmailUsedException ex, WebRequest request) {
+        return buildResponse(HttpStatus.CONFLICT, ex.getMessage(), request);
     }
 
-    @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<ErrorResponse> handleBadCredentials(BadCredentialsException ex) {
-        ErrorResponse response = new ErrorResponse(401);
-        response.setErrors(new String[]{ex.getMessage()});
-
-        return ResponseEntity.status(response.getStatus()).body(response);
-    }
-
-    @ExceptionHandler(RefreshTokenExpiredException.class)
-    public ResponseEntity<ErrorResponse> handleBadCredentials(RefreshTokenExpiredException ex) {
-        ErrorResponse response = new ErrorResponse(403);
-        response.setErrors(new String[]{ex.getMessage()});
-
-        return ResponseEntity.status(response.getStatus()).body(response);
-    }
-
-    @ExceptionHandler(AccessTokenExpiredException.class)
-    public ResponseEntity<ErrorResponse> handleBadCredentials(AccessTokenExpiredException ex) {
-        ErrorResponse response = new ErrorResponse(401);
-        response.setErrors(new String[]{ex.getMessage()});
-
-        return ResponseEntity.status(response.getStatus()).body(response);
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ApiError> handleException(UsernameNotFoundException ex, WebRequest request) {
+        return buildResponse(HttpStatus.NOT_FOUND, ex.getMessage(), request);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleBadCredentials(EntityNotFoundException ex) {
-        ErrorResponse response = new ErrorResponse(404);
-        response.setErrors(new String[]{ex.getMessage()});
-
-        return ResponseEntity.status(response.getStatus()).body(response);
+    public ResponseEntity<ApiError> handleBadCredentials(EntityNotFoundException ex, WebRequest request) {
+        return buildResponse(HttpStatus.NOT_FOUND, ex.getMessage(), request);
     }
+
+    // Default
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleBadCredentials(Exception ex, WebRequest request) {
+        return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), request);
+    }
+
+//    @ExceptionHandler(BadCredentialsException.class)
+//    public ResponseEntity<ErrorResponse> handleBadCredentials(BadCredentialsException ex) {
+//        ErrorResponse response = new ErrorResponse(401);
+//        response.addError(ex.getMessage());
+//
+//        return ResponseEntity.status(response.getStatus()).body(response);
+//    }
+//
+//    @ExceptionHandler(RefreshTokenExpiredException.class)
+//    public ResponseEntity<ErrorResponse> handleBadCredentials(RefreshTokenExpiredException ex) {
+//        ErrorResponse response = new ErrorResponse(403);
+//        response.addError(ex.getMessage());
+//
+//        return ResponseEntity.status(response.getStatus()).body(response);
+//    }
+//
+//    @ExceptionHandler(AccessTokenExpiredException.class)
+//    public ResponseEntity<ErrorResponse> handleBadCredentials(AccessTokenExpiredException ex) {
+//        ErrorResponse response = new ErrorResponse(401);
+//        response.addError(ex.getMessage());
+//
+//        return ResponseEntity.status(response.getStatus()).body(response);
+//    }
+//
 }
---
+++ src/main/java/com/trier/trier_report/dto/ApiError.java
@@ -0,0 +1,52 @@
+package com.trier.trier_report.dto;
+
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ApiError {
+    private final int status;
+    private final String path;
+    private final ArrayList<String> faults;
+    private final ArrayList<String> errors;
+    LocalDateTime timestamp;
+
+    public ApiError(int status, String errorMessage, String path) {
+        this.status = status;
+        this.path = path;
+        this.timestamp = LocalDateTime.now();
+        this.faults = new ArrayList<>();
+        this.errors = new ArrayList<String>(List.of(errorMessage));
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public ArrayList<String> getFaults() {
+        return faults;
+    }
+
+    public ArrayList<String> getErrors() {
+        return errors;
+    }
+
+    public void addFault(String fault) {
+        this.faults.add(fault);
+    }
+
+    public void addError(String error) {
+        this.errors.add(error);
+    }
+}
+
